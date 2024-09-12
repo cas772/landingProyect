115b620552b07f4310ea66df11eb0ec6731ceb02
Carlos Silva <9291cs@gmail.com>
2024-09-12 08:02:49 -0300

    Vinculando los archivos commit2

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Empresa</title>
 </head>
 <body>
-    
+<header></header>
+<section></section>
+<aside></aside>
+<footer></footer>
+<script type="text/javascript" src="js/code.js"></script>    
 </body>
 </html>
